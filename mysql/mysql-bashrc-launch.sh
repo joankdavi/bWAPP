e0eeb970babf7f13f4400ab6df423d524e4cc55c
joankdavi <joankdavi@gmail.com>
2020-06-07 10:58:27 -0500

    Update mysql-bashrc-launch.sh

--- mysql/mysql-bashrc-launch.sh
+++ mysql/mysql-bashrc-launch.sh
@@ -8,7 +8,7 @@ then
     touch /var/run/mysqld/gitpod-init.lock
 
     # initialize database structures on disk, if needed
-    [ ! -d /workspace/mysql ] && mysqld --initialize-insecure
+    [ ! -d /workspace/bWAPP/mysql ] && mysqld --initialize-insecure
 
     # launch database, if not running
     [ ! -e /var/run/mysqld/mysqld.pid ] && mysqld --daemonize
